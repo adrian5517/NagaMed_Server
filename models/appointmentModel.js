00b1d2e1b22e9ee434f461ea128273adbfa1711e
Adrian Juel Boncodin <juelboncodin@gmail.com>
2025-03-28 03:20:51 +0800

    added uuid at appointment model

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
-const appointmentSchema = new mongoose.Schema({
-  appointment_id: { type: String, unique: true, required: true },
-  patient_id: { type: String, required: true },
-  doctor_id: { type: String, required: true },
-  clinic_id: { type: String, required: true },
-  appointment_date_time: { type: Date, required: true },
-  status: { type: String, required: true, enum: ["Pending", "Confirmed", "Cancelled"] }
-}, { timestamps: true });
+const appointmentSchema = new mongoose.Schema(
+  {
+    appointment_id: { type: String, unique: true, required: true, default: uuidv4 },
+    patient_id: { type: String, required: true },
+    doctor_id: { type: String, required: true },
+    clinic_id: { type: String, required: true },
+    appointment_date_time: { type: Date, required: true },
+    status: { type: String, required: true, enum: ["Pending", "Confirmed", "Cancelled"] },
+  },
+  { timestamps: true }
+);
 
 const Appointment = mongoose.model("Appointment", appointmentSchema);
 module.exports = Appointment;
