bd1924f28397fa9b03de0b2424a052048d09494b
Adrian Boncodin <85339926+adrian5517@users.noreply.github.com>
2025-04-26 11:52:11 +0800

    Update appointmentModel.js

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,27 +1,37 @@
 const mongoose = require('mongoose');
 
-const appointmentSchema = new mongoose.Schema({
-  patient_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  doctor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
-  clinic_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Clinic', required: true },
+const AppointmentSchema = new mongoose.Schema({
+  appointment_id: {
+    type: String,
+    unique: true
+  },
+  patient_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  doctor_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Doctor',
+    required: true
+  },
+  clinic_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Clinic',
+    required: true
+  },
   appointment_date_time: {
     type: Date,
-    required: true,
-    validate: {
-      validator: function (value) {
-        return value > new Date();
-      },
-      message: 'Appointment date must be in the future.',
-    },
+    required: true
   },
   status: {
     type: String,
-    enum: ['Pending', 'Confirmed', 'Cancelled', 'Completed'],
-    default: 'Pending',
-  },
+    enum: ['Pending', 'Approved', 'Declined'],
+    default: 'Pending'
+  }
 }, { timestamps: true });
 
 // appointmentSchema.index({ clinic_id: 1, appointment_date_time: 1 });
 
-const Appointment = mongoose.model('Appointment', appointmentSchema);
-module.exports = Appointment;
+const Appointment = mongoose.model('Appointment', AppointmentSchema);
+module.exports = Appointment; controllers
