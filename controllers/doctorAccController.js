ff9bf29fc4b6b5d20f66f95ffbf64d633650ae71
Adrian Boncodin <juelboncodin@gmail.com>
2025-05-20 00:23:27 +0800

    refactor the password from login function

--- controllers/doctorAccController.js
+++ controllers/doctorAccController.js
@@ -38,8 +38,8 @@ exports.loginDoctor = async (req, res) => {
             return res.status(400).json({ message: "Doctor not found" });
         }
         
-        doctorAccSchema.methods.comparePassword = async function (enteredPassword){
-            return await bcrypt.compare(enteredPassword, this.password);
+        doctorAccSchema.methods.comparePassword = async function (password){
+            return await bcrypt.compare(password, this.password);
         }
 
         res.status(200).json({
